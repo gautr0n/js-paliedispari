5b3ccbada7574d07852d05b06b02ede7a23cbf0d
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-08 16:18:51 +0100

    palindrome

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 //palindroma
+const word = prompt(`inserire una parola`);
+console.log(word);
+
+palindrome (word);
+
+function palindrome (wordTest){
+    let wordPal = ``;
+    for (let i = wordTest.length ; i >= 0 ; i--){
+        wordPal += wordTest.toLowerCase().charAt(i);
+    }
+    if (wordPal === word.toLowerCase()){
+        console.log(`la parola inserita è palindroma`);
+        console.log(word);
+        console.log(wordPal);
+    } else {
+        console.log(`la parola inserita non è palindroma`);
+        console.log(word);
+        console.log(wordPal);
+    }
+}
